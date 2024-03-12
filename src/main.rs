5aa494458626507eda7d860a55e045b2b274a7d2
Colin Roberts <colin@primitive.xyz>
2024-03-12 09:18:57 -0600

    feat: button select future

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+#![feature(generic_arg_infer)]
 
 use embassy_executor::Spawner;
+use embassy_futures::select::{select3, select_array, Either3};
 use embassy_time::{Duration, Timer};
 use embedded_hal_async::digital::Wait;
 use esp32c6_hal::{
-    clock::{ClockControl, Clocks},
-    embassy::{self, executor::Executor},
-    gpio::{GpioPin, Output, PushPull},
+    clock::ClockControl,
+    embassy::{self},
+    gpio::{GpioPin, Input, Output, PullDown, PushPull},
     peripherals::Peripherals,
     prelude::*,
     timer::TimerGroup,
-    Delay, IO,
+    IO,
 };
 use esp_backtrace as _;
 use esp_println::println;
+
+enum Color {
+    Green,
+    Blue,
+    White,
+}
+
+struct InputButtons {
+    green: GpioPin<Input<PullDown>, 1>,
+    blue: GpioPin<Input<PullDown>, 8>,
+    white: GpioPin<Input<PullDown>, 2>,
+}
+
+impl InputButtons {
+    async fn get_input(&mut self) -> Color {
+        let either = select3(
+            self.green.wait_for_high(),
+            self.blue.wait_for_high(),
+            self.white.wait_for_high(),
+        )
+        .await;
+        match either {
+            Either3::First(_) => Color::Green,
+            Either3::Second(_) => Color::Blue,
+            Either3::Third(_) => Color::White,
+        }
+    }
+}
+
+struct OutputLeds {
+    green: GpioPin<Output<PushPull>, 18>,
+    blue: GpioPin<Output<PushPull>, 19>,
+    white: GpioPin<Output<PushPull>, 20>,
+}
 
 #[main]
 async fn main(spawner: Spawner) -> ! {
@@ -29,16 +65,47 @@ async fn main(spawner: Spawner) -> ! {
 
     // Set up the io
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut input = io.pins.gpio1.into_pull_down_input();
-    let led = io.pins.gpio0.into_push_pull_output();
+    let mut input = InputButtons {
+        green: io.pins.gpio1.into_pull_down_input(),
+        blue: io.pins.gpio8.into_pull_down_input(),
+        white: io.pins.gpio2.into_pull_down_input(),
+    };
+    let mut output = OutputLeds {
+        green: io.pins.gpio18.into_push_pull_output(),
+        blue: io.pins.gpio19.into_push_pull_output(),
+        white: io.pins.gpio20.into_push_pull_output(),
+    };
 
-    spawner.spawn(blinky(led)).unwrap();
+    // Set up a blinky task
+    let blinky_led = io.pins.gpio0.into_push_pull_output();
+    spawner.spawn(blinky(blinky_led)).unwrap();
 
+    // The main loop
     loop {
         println!("Waiting for button press...");
-        input.wait_for_high().await.unwrap();
+        let color = input.get_input().await;
         Timer::after_millis(200).await;
         println!("Button pressed!");
+        match color {
+            Color::Green => {
+                println!("Green button pressed!");
+                output.green.set_high().unwrap();
+                Timer::after(Duration::from_secs(2)).await;
+                output.green.set_low().unwrap();
+            }
+            Color::Blue => {
+                println!("Blue button pressed!");
+                output.blue.set_high().unwrap();
+                Timer::after(Duration::from_secs(2)).await;
+                output.blue.set_low().unwrap();
+            }
+            Color::White => {
+                println!("White button pressed!");
+                output.white.set_high().unwrap();
+                Timer::after(Duration::from_secs(2)).await;
+                output.white.set_low().unwrap();
+            }
+        }
     }
 }
 
